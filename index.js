57b763c5beff8ec22f3a777f7f68d0bb162f1b3a
geno91 <andrea.domenico.genovese@gmail.com>
2022-02-22 10:11:37 +0100

    feat(index2.js, inedex.js): go back to previous commit

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 const x = 'pippo';
 const env = 'develop';
-const change = 'change on main, change on develop, another change on main, another change on develop, the changes are here again from main';
+const change = 'change on main, change on develop, another change on main, another change on develop, the changes are here again from main, hello from main, hello another time. ciaooooooo';
